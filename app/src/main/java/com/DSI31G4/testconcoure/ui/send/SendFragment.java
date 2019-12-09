e1f47ded49d6652ad00f94cd0693b24476fba42f
MyprojectAndroid <mariem.felah.98@gmail.com>
2019-12-09 21:29:49 +0100

    ajouter la langue anglais et modifier l'interface info

--- app/src/main/java/com/DSI31G4/testconcoure/ui/send/SendFragment.java
+++ app/src/main/java/com/DSI31G4/testconcoure/ui/send/SendFragment.java
@@ -4,6 +4,8 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 import android.widget.TextView;
 
 import androidx.annotation.Nullable;
@@ -18,19 +20,20 @@ import com.DSI31G4.testconcoure.ui.send.SendViewModel;
 public class SendFragment extends Fragment {
 
     private SendViewModel sendViewModel;
+    public WebView mWebView;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        sendViewModel =
-                ViewModelProviders.of(this).get(SendViewModel.class);
-        View root = inflater.inflate(R.layout.fragment_send, container, false);
-        final TextView textView = root.findViewById(R.id.text_send);
-        sendViewModel.getText().observe(this, new Observer<String>() {
-            @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
-            }
-        });
+
+
+                //Corps du Fragment Actualité
+                View root=inflater.inflate(R.layout.fragment_send, container, false);
+                mWebView =(WebView)root.findViewById(R.id.webview);
+                mWebView.loadUrl("https://www.google.com");
+
+                mWebView.setWebViewClient(new WebViewClient());
+
         return root;
     }
+
 }
